3ae04e49c913255a50ac8d1c8d69f65b4c4fec97
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-04-12 09:33:46 +0900

    first imoprt

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/mattn/nostr-undelete
+
+go 1.20
+
+require github.com/nbd-wtf/go-nostr v0.16.10
+
+require (
+	github.com/SaveTheRbtz/generic-sync-map-go v0.0.0-20220414055132-a37292614db8 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.3 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/valyala/fastjson v1.6.3 // indirect
+	golang.org/x/exp v0.0.0-20221106115401-f9659909a136 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+)
+
+func main() {
+	priv := os.Args[1]
+	var pub string
+	if _, s, err := nip19.Decode(priv); err != nil {
+		log.Fatal(err)
+	} else {
+		if pub, err = nostr.GetPublicKey(s.(string)); err != nil {
+			log.Fatal(err)
+		}
+	}
+	filter := nostr.Filter{
+		Kinds:   []int{nostr.KindSetMetadata},
+		Authors: []string{pub},
+		Limit:   1,
+	}
+
+	relay, err := nostr.RelayConnect(context.Background(), "wss://nostr-relay.nokotaro.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	evs, err := relay.QuerySync(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var m map[string]any
+	err = json.Unmarshal([]byte(evs[0].Content), &m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	delete(m, "deleted")
+
+	var sk string
+	if _, s, err := nip19.Decode(priv); err != nil {
+		log.Fatal(err)
+	} else {
+		sk = s.(string)
+	}
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var ev nostr.Event
+	ev.PubKey = pub
+	ev.CreatedAt = time.Now()
+	ev.Content = string(b)
+	ev.Kind = nostr.KindSetMetadata
+	if err := ev.Sign(sk); err != nil {
+		log.Fatal(err)
+	}
+	relay.Publish(context.Background(), ev)
+}
